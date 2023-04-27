d2f2ca689914928fdabb36bab9a7175ac7515c8d
deborah-mattge <129800296+deborah-mattge@users.noreply.github.com>
2023-04-27 15:47:32 -0300

    pronto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BotaoModule } from './botao/botao.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BotaoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
